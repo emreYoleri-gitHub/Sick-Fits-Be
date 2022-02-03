42a854a89d671f9de5160f870eee8653da510cac
Emre Yoleri <emreyoleri01@gmail.com>
2022-02-03 21:26:18 +0300

    try 2

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,7 +2,7 @@ import { createTransport, getTestMessageUrl } from "nodemailer";
 
 const transport = createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
+  port: process.env.MAIL_PORT,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
